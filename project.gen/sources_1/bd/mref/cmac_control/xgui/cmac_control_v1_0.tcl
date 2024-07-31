3f7583bca485609f871e4d69fb1edfbd26bc488a
DouglasWWolf <douglaswolf@yahoo.com>
2024-07-31 14:20:04 -0700

    No commit message

--- project.gen/sources_1/bd/mref/cmac_control/xgui/cmac_control_v1_0.tcl
+++ project.gen/sources_1/bd/mref/cmac_control/xgui/cmac_control_v1_0.tcl
@@ -6,6 +6,7 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "RSFEC" -parent ${Page_0}
   ipgui::add_param $IPINST -name "TX_DIFF" -parent ${Page_0}
   ipgui::add_param $IPINST -name "TX_PRECURSOR" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "USE_SYS_RESET_OUT" -parent ${Page_0}
 
 
 }
@@ -37,6 +38,15 @@ proc validate_PARAM_VALUE.TX_PRECURSOR { PARAM_VALUE.TX_PRECURSOR } {
 	return true
 }
 
+proc update_PARAM_VALUE.USE_SYS_RESET_OUT { PARAM_VALUE.USE_SYS_RESET_OUT } {
+	# Procedure called to update USE_SYS_RESET_OUT when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.USE_SYS_RESET_OUT { PARAM_VALUE.USE_SYS_RESET_OUT } {
+	# Procedure called to validate USE_SYS_RESET_OUT
+	return true
+}
+
 
 proc update_MODELPARAM_VALUE.RSFEC { MODELPARAM_VALUE.RSFEC PARAM_VALUE.RSFEC } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -51,5 +61,10 @@ proc update_MODELPARAM_VALUE.TX_PRECURSOR { MODELPARAM_VALUE.TX_PRECURSOR PARAM_
 proc update_MODELPARAM_VALUE.TX_DIFF { MODELPARAM_VALUE.TX_DIFF PARAM_VALUE.TX_DIFF } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.TX_DIFF}] ${MODELPARAM_VALUE.TX_DIFF}
+}
+
+proc update_MODELPARAM_VALUE.USE_SYS_RESET_OUT { MODELPARAM_VALUE.USE_SYS_RESET_OUT PARAM_VALUE.USE_SYS_RESET_OUT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.USE_SYS_RESET_OUT}] ${MODELPARAM_VALUE.USE_SYS_RESET_OUT}
 }
 
--- project.gen/sources_1/bd/top_level/hdl/top_level_wrapper.v
+++ project.gen/sources_1/bd/top_level/hdl/top_level_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
-//Date        : Sat Jul 27 04:06:59 2024
+//Date        : Wed Jul 31 14:14:57 2024
 //Host        : simtool-5 running 64-bit Ubuntu 20.04.6 LTS
 //Command     : generate_target top_level_wrapper.bd
 //Design      : top_level_wrapper
